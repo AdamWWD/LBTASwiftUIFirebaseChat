1ae688185e271dbc9f0b9bba8326fb9b16e7aed2
adamwang <falco1016@gmail.com>
2022-03-02 21:31:32 +0800

    Initial Commit

---
+++ LBTASwiftUIFirebaseChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LBTASwiftUIFirebaseChat
+//
+//  Created by Woei-Deng Wang on 2022/3/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LBTASwiftUIFirebaseChat/LBTASwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTASwiftUIFirebaseChatApp.swift
+//  LBTASwiftUIFirebaseChat
+//
+//  Created by Woei-Deng Wang on 2022/3/2.
+//
+
+import SwiftUI
+
+@main
+struct LBTASwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
